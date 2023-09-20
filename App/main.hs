0d4aa1e6a4c3f0dc719ce3611f24fae7d8d70e96
GabrielRoger07 <gabrielroger4203@gmail.com>
2023-09-20 13:20:32 -0300

    Corrigindo problema de entrada do usuario invalida

--- App/main.hs
+++ App/main.hs
@@ -2,7 +2,6 @@ import RegrasDamas (validateMove, checkWin, Piece(..), Board)
 import Data.List (intersperse)
 import System.Console.Haskeline
 
-
 printBoard :: Board -> IO ()
 printBoard board = putStrLn $ unlines (legend : headerRow : map showRow (zip [0..] board))
   where
@@ -25,27 +24,31 @@ createInitialBoard =
     , [White, Empty, White, Empty, White, Empty, White, Empty]
     ]
 
-nextPlayer :: Piece -> Piece
-nextPlayer Black = White
-nextPlayer White = Black
-
 playGame :: Board -> Piece -> IO ()
 playGame board player = do
     putStrLn $ "Jogador atual: " ++ show player
     putStrLn "Informe a jogada (linhaOrigem colunaOrigem linhaDestino colunaDestino): "
     input <- runInputT defaultSettings getLine'
-    let [rowFrom, colFrom, rowTo, colTo] = map read (words input)
-    case validateMove board player rowFrom colFrom rowTo colTo of
-        Just newBoard -> do
-            printBoard newBoard
-            if checkWin newBoard player
-                then putStrLn $ "Jogador " ++ show player ++ " venceu!"
-                else playGame newBoard (nextPlayer player)
-        Nothing -> do
+    case map read (words input) of
+        [rowFrom, colFrom, rowTo, colTo] -> 
+            case validateMove board player rowFrom colFrom rowTo colTo of
+                Just newBoard -> do
+                    printBoard newBoard
+                    if checkWin newBoard player
+                        then putStrLn $ "Jogador " ++ show player ++ " venceu!"
+                        else playGame newBoard (nextPlayer player)
+                Nothing -> do
+                    putStrLn "Jogada inválida, tente novamente."
+                    playGame board player
+        _ -> do
             putStrLn "Jogada inválida, tente novamente."
             playGame board player
   where
     getLine' = getInputLine "" >>= maybe (return "") return
+
+nextPlayer :: Piece -> Piece
+nextPlayer Black = White
+nextPlayer White = Black
 
 main :: IO ()
 main = do
