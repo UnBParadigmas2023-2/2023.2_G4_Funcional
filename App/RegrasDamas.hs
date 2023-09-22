6d9d131a057470add5abd9d8a4a7df5de4ae779e
Juliana Valle <juju.pvg@gmail.com>
2023-09-21 19:32:05 -0700

    Adicionando movimento andar para trás nas peças

--- App/RegrasDamas.hs
+++ App/RegrasDamas.hs
@@ -29,10 +29,7 @@ validateMove board player rowFrom colFrom rowTo colTo
     colDiff = colTo - colFrom
     isCaptureMove = abs rowDiff == 2 && abs colDiff == 2 && isOpponentPiece (rowFrom + signum rowDiff) (colFrom + signum colDiff)
     isSimpleMove = abs rowDiff == 1 && abs colDiff == 1 && isValidDirection
-    isValidDirection
-        | player == Black = rowDiff == 1
-        | player == White = rowDiff == -1
-        | otherwise = False
+    isValidDirection = abs rowDiff == 1
     isOpponentPiece r c = r >= 0 && r < 8 && c >= 0 && c < 8 && board !! r !! c == nextPlayer player
     invalidMove = outOfBounds || notPlayerPiece || notEmpty || (not isCaptureMove && not isSimpleMove)
     makeCaptureMove = Just $ executeCaptureMove board player rowFrom colFrom rowTo colTo
